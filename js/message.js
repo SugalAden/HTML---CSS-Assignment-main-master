c4e12136f34fc0a8086cf27dc6e17906110a2d86
Sugal99 <56642911+Sugal99@users.noreply.github.com>
2023-03-24 06:10:49 +0100

    Last Commit

--- js/message.js
+++
@@ -1,3 +0,0 @@
-function submit() {
-  alert("Validation Sucess! We will be in contact with you shortly");
-}
--- submitMessage.js
+++
@@ -1,5 +0,0 @@
-
-function submit() {
-    alert("Your message has been received.\nWe will contact you as soon as possible!");
-  }
-  
